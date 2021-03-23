64bb53e4ebc0da362af72c967a32576442c12e1c
kenshin1025 <sukoyakamind1025@gmail.com>
2021-03-23 15:44:22 +0900

    DB接続までした

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
+)
+
+// わかりやすさのためにこうしてるけどセキュリティ的にはenvとかから取り出すようにしたほうが良いと思います
+const (
+	DBUserName = "root"
+	DBPassword = "passw0rd"
+	DBHost     = "sample_db"
+	DBPort     = "3306"
+	DBName     = "sample"
 )
 
 func main() {
 	fmt.Printf("Starting server at 'http://localhost:8080'\n")
 
-	// dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", DBUserName, DBPassword, DBHost, DBPort, DBName)
 
-	// db, err := sql.Open("mysql", dsn)
-	// if err != nil {
-	// 	log.Fatal("open failed")
-	// }
-	// defer db.Close()
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 }
